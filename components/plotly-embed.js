f1318fa466546fd155b33a938c24e199591de365
Jane Hoffswell <jhoffs@cs.washington.edu>
2021-04-12 10:01:49 -0700

    Plotly Example
    Add the Plotly example from Kalyani and update the text. Update the PlotlyEmbed component to set the "showlink" option to false by default.

--- components/plotly-embed.js
+++ components/plotly-embed.js
@@ -3,14 +3,16 @@ const React = require('react');
 class PlotlyEmbed extends React.Component {
   render() {
     const { hasError, idyll, updateProps, ...props } = this.props;
+    console.log("SHOW", this.props.showlink)
+    var showLink = "?showlink=" + (this.props.showlink ? true : false)
     return (
       <iframe 
         scrolling={"no"}
         style={{border:"none"}}
         seamless={"seamless"}
-        src={this.props.link} 
-        height={this.props.height} //"525" 
-        width={this.props.width} //"100%"
+        src={this.props.link + showLink}
+        height={this.props.height}
+        width={this.props.width}
       />
     );
   }
